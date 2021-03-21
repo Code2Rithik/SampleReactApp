5b9d320d088a271a39b64bb23f6f59c6b2a8fc5a
Rithik Samanthula <talk2rithiks@gmail.com>
2021-03-21 12:55:33 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>This is a heading</h1>
+    <ul>
+      <li>This is an item</li>
+      <li>This is an item</li>
+      <li>This is an item</li>
+    </ul>
+  </div>,
+
+  document.getElementById("root")
+);
